566e0fa20ed3d8184c71f8100186b148a3316710
Adam301c <adam.chenyx@gmail.com>
2022-08-31 19:30:03 +0800

    uploading unity app version_1.0

---
+++ app_version_1.0/Assets/Scripts/Panel.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start : MonoBehaviour
+{
+    public GameObject panel1;
+    // Start is called before the first frame update
+    void Starts()
+    {
+        panel1.SetActive(true);
+    }
+
+}
