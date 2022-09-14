ae6803d7efb5f7f9fd4c6a96c8c15b4e41b6d729
Adam301c <adam.chenyx@gmail.com>
2022-09-14 15:18:14 +0800

    added login features

--- app_version_1.0/Assets/AuthManager.cs
+++ app_version_1.0/Assets/AuthManager.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 using Firebase;
 using Firebase.Auth;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class AuthManager : MonoBehaviour
 {
+    public PanelSwitch pscript;
+    
     public FirebaseAuth auth;
     public FirebaseUser User;
     public DependencyStatus dependencyStatus;
+    public static bool is_logged = false;
 
     [Header("Register")]
     public TMP_InputField username;
     public TMP_InputField email;
     public TMP_InputField passwordr;
     public TMP_Text warningRegisterText;
+    public TMP_Text warningLoginText;
+    public TMP_Text confirmLoginText;
+    public TMP_Text welcome;
+    public TMP_InputField usernamel;
+    public TMP_InputField passwordl;
 
     void Awake()
     {
@@ -34,15 +43,84 @@ public class AuthManager : MonoBehaviour
             }
         });
     }
+    public void LoginButton()
+    {
+        //Call the login coroutine passing the email and password
+        StartCoroutine(Login(usernamel.text, passwordl.text));
+    }
     private void InitializeFirebase()
     {
         Debug.Log("Setting up Firebase Auth");
         //Set the authentication instance object
         auth = FirebaseAuth.DefaultInstance;
     }
+
+    private void clearLogin()
+    {
+        usernamel.text = "";
+        passwordl.text = "";
+    }
+
+    public void logoutButton()
+    {
+        is_logged=false;
+        auth.SignOut();
+        pscript.clear();
+        pscript.Profile();
+        confirmLoginText.text = "";
+        clearLogin();
+    }
     public void RegisterButton()
     {
         StartCoroutine(Register(email.text,passwordr.text,username.text));
+    }
+    private IEnumerator Login(string _email, string _password)
+    {
+        //Call the Firebase auth signin function passing the email and password
+        var LoginTask = auth.SignInWithEmailAndPasswordAsync(_email, _password);
+        //Wait until the task completes
+        yield return new WaitUntil(predicate: () => LoginTask.IsCompleted);
+
+        if (LoginTask.Exception != null)
+        {
+            //If there are errors handle them
+            Debug.LogWarning(message: $"Failed to register task with {LoginTask.Exception}");
+            FirebaseException firebaseEx = LoginTask.Exception.GetBaseException() as FirebaseException;
+            AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
+
+            string message = "Login Failed!";
+            switch (errorCode)
+            {
+                case AuthError.MissingEmail:
+                    message = "Missing Email";
+                    break;
+                case AuthError.MissingPassword:
+                    message = "Missing Password";
+                    break;
+                case AuthError.WrongPassword:
+                    message = "Wrong Password";
+                    break;
+                case AuthError.InvalidEmail:
+                    message = "Invalid Email";
+                    break;
+                case AuthError.UserNotFound:
+                    message = "Account does not exist";
+                    break;
+            }
+            warningLoginText.text = message;
+        }
+        else
+        {
+            //Logged In
+            is_logged = true;
+            User = LoginTask.Result;
+            warningLoginText.text = "";
+            confirmLoginText.text = "Logged In";
+            welcome.text = "Welcome "+ User.DisplayName;
+            pscript.clear();
+            pscript.Logged();
+            
+        }
     }
     private IEnumerator Register(string _email, string _password, string _username)
     {
@@ -81,6 +159,9 @@ public class AuthManager : MonoBehaviour
                     case AuthError.EmailAlreadyInUse:
                         message = "Email Already In Use";
                         break;
+                    case AuthError.InvalidEmail:
+                        message = "Poor Email Format";
+                        break;
                 }
                 warningRegisterText.text = message;
             }
@@ -102,7 +183,7 @@ public class AuthManager : MonoBehaviour
 
                     if (ProfileTask.Exception != null)
                     {
-                        //If there are errors handle them
+                        //error handling
                         Debug.LogWarning(message: $"Failed to register task with {ProfileTask.Exception}");
                         FirebaseException firebaseEx = ProfileTask.Exception.GetBaseException() as FirebaseException;
                         AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
@@ -110,9 +191,10 @@ public class AuthManager : MonoBehaviour
                     }
                     else
                     {
-                        //Username is now set
-                        //Now return to login screen
-                        
+                        //Username set
+                    
+                        pscript.clear();
+                        pscript.Profile();
                         warningRegisterText.text = "";
                     }
                 }
