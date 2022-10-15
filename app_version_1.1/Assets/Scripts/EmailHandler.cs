625e548d02570c5cca5c13f4e8b5e1393348c6e9
wkj3 <wongkj3@gmail.com>
2022-10-15 14:28:53 +0800

    email fixes
    fixed browser not showing files, changed  email field to dropdown

--- app_version_1.1/Assets/Scripts/EmailHandler.cs
+++ app_version_1.1/Assets/Scripts/EmailHandler.cs
@@ -6,12 +6,14 @@ using System.Net;
 using System.Net.Mail;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
+using TMPro;
 
 public class EmailHandler : MonoBehaviour
 {
     public InputField bodyMessage;
-    public InputField recipientEmail;
 	public InputField subjectLine;
+	public TMP_Dropdown recipientEmailSelect;
+	public string recipientEmail;
 
 	public void SendEmail()
     {
@@ -21,15 +23,28 @@ public class EmailHandler : MonoBehaviour
             SmtpClient smtpC = new SmtpClient("smtp.gmail.com");
             //From address to send email
             mail.From = new MailAddress("wacrhapptest@gmail.com");
-            //To address to send email
-            mail.To.Add(recipientEmail.text);
+            //To address to send email (0 = first option, 1 = second option, 2 = third option)
+			if (recipientEmailSelect.value == 0)
+				{
+					recipientEmail = "wongkj3@gmail.com";
+				}
+			if (recipientEmailSelect.value == 1)
+				{
+					recipientEmail = "email2@gmail.com";
+				}
+			if (recipientEmailSelect.value == 2)
+				{
+					recipientEmail = "email3@gmail.com";
+				}
+            mail.To.Add(recipientEmail);
             mail.Body = bodyMessage.text;
             mail.Subject = subjectLine.text;
 			
-			Attachment attachment = null;
-			attachment = new Attachment(EmailFileHandler.destinationPath);
-			mail.Attachments.Add(attachment);
-			
+			if (EmailFileHandler.destinationPath != null){
+				Attachment attachment = null;
+				attachment = new Attachment(EmailFileHandler.destinationPath);
+				mail.Attachments.Add(attachment);
+			}
             smtpC.Port = 587;
             //Credentials for From address
             smtpC.Credentials =(System.Net.ICredentialsByHost) new System.Net.NetworkCredential("wacrhapptest@gmail.com", "yxfffkqosdkicftt");
